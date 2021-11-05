bff2ed7397771d1707eab5c49d48673af9154c12
Joana Cruz <joana_c_ca@hotmail.com>
2021-11-05 11:54:35 +0000

    Changed place to start

--- game.js
+++ game.js
@@ -11,12 +11,12 @@ class Game {
       "./images/plankton.png",
       "./images/MrsPuff.png",
     ];
-    this.obstacleArr = [new Obstacle("./images/snail_char.png", 500)];
-    this.obstaclesDistance = 100;
+    this.obstacleArr = [new Obstacle("./images/snail_char.png", 875)];
+    this.obstaclesDistance = 120;
     this.isGameOver = false;
     this.obstacleSpeed = 2.5;
     this.bobDropSpeed = 2;
-    this.randomRange = [500, 600, 700];
+    this.randomRange = [850, 900, 950];
     this.score = 0;
     this.scoreTag = document.querySelector("span");
     this.scoreTag.innerHTML = this.score;
